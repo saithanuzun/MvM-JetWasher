4ef2254903b06997ad9152ab1964ac33767aa351
sait han uzun <saithan.uzun@gmail.com>
2024-07-27 23:59:32 +0100

    version 0.1

---
+++ MvM-JetWasher/Models/ContactForm.cs
@@ -0,0 +1,8 @@
+
+public class ContactForm
+{
+    public string Name { get; set; }
+    public string MobileNumber { get; set; }
+    public string Service { get; set; }
+    public string Comment { get; set; }
+}
--- MvM-JetWasher/obj/Debug/net8.0/MvM-JetWasher.AssemblyInfo.cs
+++ MvM-JetWasher/obj/Debug/net8.0/MvM-JetWasher.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvM-JetWasher")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99df0f6d38153401c9fae02f33b2d1827dc5b71e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvM-JetWasher")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvM-JetWasher")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

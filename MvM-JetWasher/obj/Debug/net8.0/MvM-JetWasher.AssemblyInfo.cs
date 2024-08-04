460c6e5d09f7db834dc5e2b413a63e7b7417d0a6
sait han uzun <saithan.uzun@gmail.com>
2024-08-04 23:04:00 +0100

    deleted workflw

--- MvM-JetWasher/obj/Debug/net8.0/MvM-JetWasher.AssemblyInfo.cs
+++ MvM-JetWasher/obj/Debug/net8.0/MvM-JetWasher.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvM-JetWasher")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f9a80b3afed31289650b8070effea59e14a07b72")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c4ca9dfeaade9fd756ca8cde995f2c8b4f78beaf")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvM-JetWasher")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvM-JetWasher")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

927fd9a5061db689eada7c80987c0830e977cf1a
sait han uzun <saithan.uzun@gmail.com>
2024-08-07 23:06:35 +0100

    whatsapp

--- MvM-JetWasher/obj/Debug/net8.0/MvM-JetWasher.AssemblyInfo.cs
+++ MvM-JetWasher/obj/Debug/net8.0/MvM-JetWasher.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MvM-JetWasher")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ed682355fd1782df12221d23f3f07259b25a3bba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d78463436261dfa249caf95e3bf6596ec32e4d8f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MvM-JetWasher")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MvM-JetWasher")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
